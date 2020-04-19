05a9d8207500ba1d035ce0b05cecee583d5960e5
ujjwal <ujjwalkumarprem@gmail.com>
2020-04-19 12:45:13 +0530

    add git ignore,procfile

--- app.js
+++ app.js
@@ -186,7 +186,13 @@ app.post("/compose", function(req, res) {
         });
       });
 
+      let port = process.env.PORT;
+      if (port == null || port == "") {
+        port = 3000;
+      }
+      
 
-      app.listen(3000, function() {
+
+      app.listen(port, function() {
         console.log("Server started on port 3000");
       });
